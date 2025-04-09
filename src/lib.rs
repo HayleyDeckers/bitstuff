de5a9ca95a5732a8cb23082e0907388e18c086bb
Hayley <git@codingcat.nl>
2025-04-09 17:27:39 +0200

    🙅‍♀️0️⃣ Add support for NonZeroUn

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 pub mod ints;
+use std::num::{NonZeroU128, NonZeroU16, NonZeroU32, NonZeroU64, NonZeroU8};
+
 pub use bitstuff_macros::*;
 
 pub trait FromLowBits<T> {
@@ -192,4 +194,88 @@ pub trait TryFromBits: Bits + Sized {
 
 impl<S: TryFromBits> Bits for Result<S, S::From> {
     const N_BITS: u32 = S::N_BITS;
+}
+
+impl Bits for NonZeroU8 {
+    const N_BITS: u32 = 8;
+}
+
+impl TryFromBits for NonZeroU8 {
+    type From = u8;
+    fn try_from_bits(bits: u8) -> Result<Self, Self::From> {
+        NonZeroU8::new(bits).ok_or(bits)
+    }
+}
+
+impl Bits for NonZeroU16 {
+    const N_BITS: u32 = 16;
+}
+
+impl TryFromBits for NonZeroU16 {
+    type From = u16;
+    fn try_from_bits(bits: u16) -> Result<Self, Self::From> {
+        NonZeroU16::new(bits).ok_or(bits)
+    }
+}
+impl Bits for NonZeroU32 {
+    const N_BITS: u32 = 32;
+}
+
+impl TryFromBits for NonZeroU32 {
+    type From = u32;
+    fn try_from_bits(bits: u32) -> Result<Self, Self::From> {
+        NonZeroU32::new(bits).ok_or(bits)
+    }
+}
+impl Bits for NonZeroU64 {
+    const N_BITS: u32 = 64;
+}
+
+impl TryFromBits for NonZeroU64 {
+    type From = u64;
+    fn try_from_bits(bits: u64) -> Result<Self, Self::From> {
+        NonZeroU64::new(bits).ok_or(bits)
+    }
+}
+
+impl Bits for NonZeroU128 {
+    const N_BITS: u32 = 128;
+}
+
+impl TryFromBits for NonZeroU128 {
+    type From = u128;
+    fn try_from_bits(bits: u128) -> Result<Self, Self::From> {
+        NonZeroU128::new(bits).ok_or(bits)
+    }
+}
+
+impl ToBits for NonZeroU8 {
+    type To = u8;
+    fn to_bits(self) -> u8 {
+        self.get()
+    }
+}
+impl ToBits for NonZeroU16 {
+    type To = u16;
+    fn to_bits(self) -> u16 {
+        self.get()
+    }
+}
+impl ToBits for NonZeroU32 {
+    type To = u32;
+    fn to_bits(self) -> u32 {
+        self.get()
+    }
+}
+impl ToBits for NonZeroU64 {
+    type To = u64;
+    fn to_bits(self) -> u64 {
+        self.get()
+    }
+}
+impl ToBits for NonZeroU128 {
+    type To = u128;
+    fn to_bits(self) -> u128 {
+        self.get()
+    }
 }
